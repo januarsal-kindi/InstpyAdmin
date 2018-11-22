adfa2488245ddfc3bb54a72d63458f027015f923
AD\januar.kindi <yayan.yayan98@gmail,com>
2018-11-22 14:58:13 +0700

    doing all rght

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React ,{Fragment} from 'react';
+const header = (props) => {
+
+    return(
+        <Fragment>
+            <h1>Header</h1>
+        </Fragment>
+    );
+}
+
+export default header;
